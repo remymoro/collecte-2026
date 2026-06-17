2dde1cdec402845d23bd1ad94758d3d89510eeab
remymoro <moro.remy879@gmail.com>
2026-06-17 10:11:33 +0200

    fix(vitest): watch false pour éviter le mode watch en run-many

--- packages/api/vitest.config.ts
+++ packages/api/vitest.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'node',
+    watch: false,
     include: ['src/**/*.spec.ts'],
     passWithNoTests: true,
     coverage: {
